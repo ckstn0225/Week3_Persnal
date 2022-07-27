6eb41ace23620e0e1f194c2d0b35c89cfbba1142
ckstn0225 <ckstn0225@gmail.com>
2022-07-27 16:11:07 +0900

    내림차순 정렬 추가

--- src/main/java/com/sparta/java3w/Service/PostGetService.java
+++ src/main/java/com/sparta/java3w/Service/PostGetService.java
@@ -4,6 +4,7 @@ import com.sparta.java3w.Dto.PostGetDto;
 import com.sparta.java3w.Entity.Post;
 import com.sparta.java3w.Repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -16,12 +17,13 @@ public class PostGetService {
     private final PostRepository postRepository;
     //    글 전체 조회 사용  [GET api/posts]
     public List<PostGetDto> onlytad() {
-        List<PostGetDto> postGetDtos1 = new ArrayList<>();
+        List<Post> tours = postRepository.findAll(Sort.by(Sort.Direction.DESC, "createdAt"));
+        List<PostGetDto> GetDto = new ArrayList<>();
         for (int i = 0; i < postRepository.count(); i++) {
-            Post postinfo = postRepository.findAll().get(i);
+            Post postinfo = tours.get(i);
             PostGetDto postGetDto = new PostGetDto(postinfo.getTitle(), postinfo.getAuther(), postinfo.getCreatedAt());
-            postGetDtos1.add(postGetDto);
+            GetDto.add(postGetDto);
         }
-        return postGetDtos1;
+        return GetDto;
     }
 }
